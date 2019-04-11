db2482a8af5c0812de568c21d988301913011ff6
Simon <simon.leonard@sfr.fr>
2019-04-11 15:08:58 +0200

    ajout app.py pour tout démarrer

---
+++ app.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3    
+# -*- coding: utf-8 -*-
+
+import website
+from threading import Thread
+
+webserver = Thread(target=website.run, daemon=False)
+webserver.start()
+webserver.join()
+
